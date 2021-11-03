df763399e56050cd5cc4065b0015ffcdf410a28f
Oliver <oliverhood15@gmail.com>
2021-11-03 18:25:57 +0000

    Update print-chainid.sh
    Corrected typo in line 4

--- print-chainid.sh
+++ print-chainid.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 for file in /serv/data/af2/cleanpdbstructures/*.pdb
 do
-for agfile in "grep ;CHAIN A' $file"
+for agfile in "grep 'CHAIN A' $file"
 	do
 	chainid=$(grep 'CHAIN A' $file | awk '{print $5}')
 	echo $chainid
