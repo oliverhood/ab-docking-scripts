abb4b1bd1666283315f92260087916c6acf83ffe
Oliver <oliverhood15@gmail.com>
2021-11-03 19:14:14 +0000

    Update get-process-antigen.sh
    Was missing 'fi' to end the if statement, oops.

--- get-process-antigen.sh
+++ get-process-antigen.sh
@@ -7,4 +7,5 @@ then
 :
 else
 pdbgetchain $chainid $file | pdbtranslate -x $(($RANDOM%50-25)) -y $(($RANDOM%50-25)) -z $(($RANDOM%50-25)) | pdbrotate -x $((($RANDOM%270)+45)) -y $((($RANDOM%270)+45)) -z $((($RANDOM%270)+45)) > $(basename ${file##*/pdb} .pdb)_Ag.pdb
+fi
 done
