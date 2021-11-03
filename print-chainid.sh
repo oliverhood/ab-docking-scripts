5774b489339c81bc2f8cb50e9ef8d6be2c23b040
Oliver <oliverhood15@gmail.com>
2021-11-03 18:32:07 +0000

    Update print-chainid.sh
    Added if statement to for loop to check if $chainid is empty

--- print-chainid.sh
+++ print-chainid.sh
@@ -1,9 +1,11 @@
 #! /bin/sh
 for file in /serv/data/af2/cleanpdbstructures/*.pdb
 do
-for agfile in "grep 'CHAIN A' $file"
-	do
-	chainid=$(grep 'CHAIN A' $file | awk '{print $5}')
-	echo $chainid
-	done
+chainid=$(grep 'CHAIN A' $file | awk '{print $5}')
+if [-z "$chainid"]
+then
+:
+else
+echo $chainid
+fi
 done
