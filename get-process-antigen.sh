223b5cb931d7d57280cea8e18e3371bd98c8143f
Oliver <oliverhood15@gmail.com>
2021-11-03 11:31:49 +0000

    Update get-process-antigen.sh

--- get-process-antigen.sh
+++ get-process-antigen.sh
@@ -4,3 +4,4 @@ do
 grep 'CHAIN A' /serv/data/af2/cleanpdbstructures/$file | awk '{print $5}'
 pdbgetchain $chainid /serv/data/af2/cleanpdbstructures/$file | pdbtranslate -x $(($RANDOM%50-25)) -y $(($RANDOM%50-25)) -z $(($RANDOM%50-25)) | pdbrotate -x $((($RANDOM%270)+45)) -y $((($RANDOM%270)+45)) -z $((($RANDOM%270)+45)) > $file_Ag.pdb
 done
+#adding comment line for test git push/pull
