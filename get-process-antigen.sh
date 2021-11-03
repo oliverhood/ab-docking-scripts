b0744e4cf99f3ff1e45c63dd3de7e05fbde9c9eb
Oliver <oliverhood15@gmail.com>
2021-11-03 19:32:32 +0000

    Update get-process-antigen.sh
    Removed elif statement for $chainid being longer than 1, didn't work (variable was a string not an intiger). Not Sure how I'd fix this so have removed.

--- get-process-antigen.sh
+++ get-process-antigen.sh
@@ -5,9 +5,6 @@ chainid=$(grep 'CHAIN A' $file | awk '{print $5}')
 if [ -z "$chainid" ]
 then
 :
-elif [ "$chainid" -ge 1 ]
-then
-:
 else
 pdbgetchain $chainid $file | pdbtranslate -x $(($RANDOM%50-25)) -y $(($RANDOM%50-25)) -z $(($RANDOM%50-25)) | pdbrotate -x $((($RANDOM%270)+45)) -y $((($RANDOM%270)+45)) -z $((($RANDOM%270)+45)) > $(basename ${file##*/pdb} .pdb)_Ag.pdb ;
 fi
