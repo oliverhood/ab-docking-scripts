0580e6cfc41b703c84e8a95718ba02d23d82be78
Oliver <oliverhood15@gmail.com>
2021-11-04 17:55:22 +0000

    Created split-antibody-antigen-files.py and split-antibody-antigen-files_lib.py
    Created the files split-antibody-antigen-files.py and split-antibody-antigen-files_lib.py. These files will eventually host the python script version of get-process-antigen.sh (when it is written).

---
+++ split-antibody-antigen-files_lib.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Program: split-antibody-antigen-files
+File:    split-antibody-antigen-files.py
+
+Version: V1.0
+Date: 04.11.21
+Function: Extract and process antigen and antibody chains from a PDB file 
+          for input to docking algorithms.
+
+Author: Oliver E. C. Hood
+
+--------------------------------------------------------------------------
+
+Description:
+============
+This program takes a PDB file containing the structure of an antibody (that
+may or may not be bound by an antigen(s)) and returns the antibody and 
+antigen(s) chains as separate PDB files. The antigen chain is processed 
+(randomly rotated and transformed) before being written to the new PDB 
+file.
+
+--------------------------------------------------------------------------
+
+Usage:
+======
+split-antibody-antigen-files PDBFILE
+
+--------------------------------------------------------------------------
+
+Revision History:
+=================
+V1.0   04.11.21   Original   By: OECH
+"""
+
+#*************************************************************************
+
+# Import Libraries
+from bioptools import (pdbgetchain, pdbtranslate, pdbrotate)
