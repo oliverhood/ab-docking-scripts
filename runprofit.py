6f4b42d043bf5332b5e8046465dbcdd8fd20e2d8
Oliver Hood <zcbtoec@ucl.ac.uk>
2021-11-10 11:40:56 +0000

    Created runprofit.py and runprofit_lib.py files, currently empty

---
+++ runprofit.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Program: runprofit
+File:    runprofit.py
+
+Version: V1.0
+Date:    
+Function: Process the output files of docking algorithms run on split antibody/antigen structures to compare them to the original antibody/antigen structures using ProFit.
+
+Author: Oliver E. C. Hood
+
+--------------------------------------------------------------------------
+
+Description:
+============
+
+
+--------------------------------------------------------------------------
+
+Usage:
+======
+
+
+--------------------------------------------------------------------------
+
+Revision History:
+=================
+
+
+"""
+
+#*************************************************************************
+
+# Import Libraries
+
