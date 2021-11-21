6fa7b6102b2bbf5f05f6b57934c87c676afe7296
oliverhood <51133654+oliverhood@users.noreply.github.com>
2021-11-21 17:44:12 +0000

    Formatting fixes

--- getresultfiles.sh
+++ getresultfiles.sh
@@ -1,35 +1,35 @@
 #! /bin/sh
-"""
-Program: getresultfiles
-File:    getresultfiles.sh
+#"""
+#Program: getresultfiles
+#File:    getresultfiles.sh
 
-Version:  V1.0
-Date:     21.11.2021
-Function: Extract result PDB files from directories created by testdockingprogs_master.py script into new directory 'result_files'
+#Version:  V1.0
+#Date:     21.11.2021
+#Function: Extract result PDB files from directories created by #testdockingprogs_master.py script into new directory 'result_files'
 
-Author: Oliver E. C. Hood
+#Author: Oliver E. C. Hood
 
---------------------------------------------------------------------------
+#--------------------------------------------------------------------------
 
-Description:
-============
-This program moves the PDB files created by multiple runs in testmegadock_vs_megadockblocked script into a single directory with runs labelled to compare docked structure results in PyMol.
+#Description:
+#============
+#This program moves the PDB files created by multiple runs in testmegadock_vs_megadockblocked script into a single directory with runs labelled to compare docked structure results in PyMol.
 
---------------------------------------------------------------------------
+#--------------------------------------------------------------------------
 
-Usage:
-======
+#Usage:
+#======
 
-getresultfiles.sh
+#getresultfiles.sh
 
-N.b. call this script in directory that testmegadock_vs_megadockblocked was run in
+#N.b. call this script in directory that testmegadock_vs_megadockblocked was run in
 
---------------------------------------------------------------------------
+#--------------------------------------------------------------------------
 
-Revision History:
-=================
+#Revision History:
+#=================
 
-"""
+#"""
 #*************************************************************************
 # Make Directory to put docked structures in
 mkdir result_files
