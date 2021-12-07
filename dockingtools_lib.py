bb42f97634f7c6d1e00569dd923aea529cd9d647
oliverhood <51133654+oliverhood@users.noreply.github.com>
2021-12-07 18:13:09 +0000

    Added get_date(), get_time(), and evaluate_results() functions

--- dockingtools_lib.py
+++ dockingtools_lib.py
@@ -27,6 +27,8 @@ V1.0   03.12.2021   Original   By: OECH
 
 # Import Libraries
 
+import subprocess
+import time
 
 #*************************************************************************
 
@@ -81,3 +83,91 @@ def writefile(filename, list):
    with open(str(filename), "w") as file:
       for line in list:
          file.write("%s\n" % line)
+
+#*************************************************************************
+
+def get_time():
+   """
+   Get the current date and time.
+
+   """
+   # Get time
+   current_time = time.strftime(r"%d.%m.%Y   %H:%M:%S", time.localtime())
+   # Return time
+   return current_time
+
+#*************************************************************************
+
+def get_date():
+   """
+   Get the current date.
+
+   """
+   # Get date
+   current_date = time.strftime(r"%d.%m.%Y", time.localtime())
+   # Return date
+   return current_date
+
+#*************************************************************************
+
+def evaluate_results(OG_file, *args, single_file=True, method='Method name not specified'):
+   """
+   Take either a single docked antibody/antigen structure, or separate antibody and antigen structures as input, run the relevant runprofit script on the structures and output the results into a results file. (Will later add functionality to determine the proportion of correctly predicted contacts)
+
+   >>> evaluate_results('test8_OG.pdb', 'test8_single.pdb')
+   ['Docking test on test8_OG.pdb   07.12.2021', '', 'Method name not specified', 'All atoms RMSD:  10.751', 'CA atoms RMSD:   10.572']
+   >>> evaluate_results('test8_OG.pdb', 'test8_ab.pdb', 'test8_Dag.pdb')
+   ['Docking test on test8_OG.pdb   07.12.2021', '', 'Method name not specified', 'All atoms RMSD:  1.652', 'CA atoms RMSD:   1.622']
+
+   """
+   # Get date
+   current_date = get_date()
+   # Create results file header
+   header = "Docking test on " + OG_file + "   " + current_date
+   spacer = ""
+   # Initialise results list
+   dockingresults = [header, spacer]
+   # Check whether input is single file (antibody+antigen) or separate files (antibody, antigen)
+   if len(args) > 1:
+      single_file=False
+   # If the input is a single PDB file containing both the antibody and antigen chains
+   if single_file:
+      # Set Title
+      method_title = f"{method}"      
+      # Define the docked_file
+      docked_file = args[0]
+      # Run the relevant profit script, capture results in 'result'
+      result = subprocess.check_output([f"~/ab-docking-scripts/runprofit_single.py {OG_file} {docked_file}"], shell=True)
+      result = str(result, 'utf-8')
+      # Extract the result lines from output
+      contents = result.split('\n')
+      all_atoms = contents[0]
+      CA_atoms = contents[1]
+      # Add method, results to dockingresults
+      dockingresults += [method_title]
+      dockingresults += [all_atoms]
+      dockingresults += [CA_atoms]
+      # Add spacer
+      dockingresults += ""
+   # If the input contains separate PDB files for the antibody and docked antigen chains
+   if not single_file:
+      # Set Title
+      method_title = f"{method}"
+      # Define the input files
+      Ab_file = args[0]
+      Dag_file = args[1]
+      # Run the relevant profit script, capture results in 'result'
+      result = subprocess.check_output([f"~/ab-docking-scripts/runprofit.py {OG_file} {Ab_file} {Dag_file}"], shell=True)
+      result = str(result, 'utf-8')
+      # Extract the result lines from output
+      contents = result.split('\n')
+      all_atoms = contents[0]
+      CA_atoms = contents[1]
+      # Add method, results to dockingresults
+      dockingresults += [method_title]
+      dockingresults += [all_atoms]
+      dockingresults += [CA_atoms]
+      # Add spacer
+      dockingresults += ""
+   # Return docking results
+   return dockingresults
