df901676a317536bc111c10fe2cf708c749baf5d
oliverhood <51133654+oliverhood@users.noreply.github.com>
2021-12-17 13:12:38 +0000

    Changed script to work in docking_results directory

--- run_testdockingprogs_master.sh
+++ run_testdockingprogs_master.sh
@@ -1,40 +1,44 @@
 #! /bin/sh
-"""
+#"""
 
-Program: testdockingprogs_shell
-File:    testdockingprogs_shell.sh
+#Program: testdockingprogs_shell
+#File:    testdockingprogs_shell.sh
 
-Version:   V1.0
-Date:      15.12.2021
-Function:  Wrapper for running testdockingprogs_master.py on a full directory of PDB files.
+#Version:   V1.0
+#Date:      15.12.2021
+#Function:  Wrapper for running testdockingprogs_master.py on a full directory of PDB files.
  
-Author: Oliver E. C. Hood
+#Author: Oliver E. C. Hood
 
---------------------------------------------------------------------------
+#--------------------------------------------------------------------------
 
-Description:
-============
-This script loops through every PDB file in a given directory, uses them as input for testdockingprogs_master.py, then concatenates the result file for each run into a single file.
+#Description:
+#============
+#This script loops through every PDB file in a given directory, uses them as input for testdockingprogs_master.py, then concatenates the result file for each run into a single file.
  
---------------------------------------------------------------------------
+#--------------------------------------------------------------------------
 
-Usage:
-======
+#Usage:
+#======
 
 
--------------------------------------------------------------------------
-"""
+#-------------------------------------------------------------------------
+#>"""
 #*************************************************************************
 
 # Make directory for docking results
 mkdir ./docking_results
 
 # Get current date
-now=$(date + '%m/%d/%Y')
+now=$(date +%m_%d_%Y)
 
 # Loop through every PDB file in current directory
 for file in ./*.pdb
    do
+      # Copy file to docking results directory
+      cp $file ./docking_results
+      # Move to docking results directory
+      cd ./docking_results
       # Run testdockingprogs_master with docking_results as the output directory
       ~/ab-docking-scripts/testdockingprogs_master.py $file ./docking_results
       # Get the file name (less .pdb)
