ca821ac771efbc6df640375f182efa6702a8bcab
oliverhood <51133654+oliverhood@users.noreply.github.com>
2022-01-26 12:32:16 +0000

    Hashed out program prompt lines so program will run properly in the meantime

--- testdockingprogs_master.py
+++ testdockingprogs_master.py
@@ -104,13 +104,16 @@ Rosetta_ag_res = []
 # Prompt user for programs to run
 
 # Megadock
-run_megadock_bool = program_prompt('Megadock')
+run_megadock_bool = True
+#run_megadock_bool = program_prompt('Megadock')
 
 # Piper
-run_piper_bool = program_prompt('Piper')
+run_piper_bool = True
+#run_piper_bool = program_prompt('Piper')
 
 # Rosetta
-run_rosetta_bool = program_prompt('Rosetta')
+run_rosetta_bool = True
+#run_rosetta_bool = program_prompt('Rosetta')
 
 #*************************************************************************
 
