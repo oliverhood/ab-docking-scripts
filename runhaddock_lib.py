e91cc2c0394ac1e4dfda4cba9063530b739625e3
oliverhood <51133654+oliverhood@users.noreply.github.com>
2022-02-15 21:38:27 +0000

    Added functions to runhaddock_lib

--- runhaddock_lib.py
+++ runhaddock_lib.py
@@ -32,3 +32,39 @@ V1.0   15.02.22   Original   By: OECH
 #*************************************************************************
 
 # Import libraries
+import subprocess
+from dockingtools_lib import writefile
+
+#*************************************************************************
+
+def clean_inputs(antibody, antigen, ab_filename, ag_filename):
+   """
+   Clean input files for entry into haddock.
+   """
+   print("Cleaning input files...", end='')
+   # clean antibody file using pdb_chain and pdb_seg
+   subprocess.run([f"~/DockingSoftware/pdb-tools/pdbtools pdb_chain.py {antibody} | ~/DockingSoftware/pdb-tools/pdbtools/pdb_seg.py | pdbrenum > {ab_filename}_clean.pdb"], shell=True)
+   # Clean antigen file using pdb_chain and pdb_seg
+   subprocess.run([f"~/DockingSoftware/pdb-tools/pdbtools pdb_chain.py {antigen} | ~/DockingSoftware/pdb-tools/pdbtools/pdb_seg.py > {ag_filename}_clean.pdb"], shell=True)
+   print("Done")
+
+#*************************************************************************
+
+def generate_unambig_tbl(ab_filename):
+   # Define clean antibody filename
+   ab_clean = ab_filename + "_clean.pdb"
+   # Run restrain_bodies script on antibody file to generate unambig restraints table
+   subprocess.run([f"~/DockingSoftware/haddock-tools/restrain_bodies.py {ab_clean} > antibody-antigen-unambig.tbl"], shell=True)
+
+#*************************************************************************
+
+def generate_run_param(ab_filename, ag_filename, OUTPath):
+   """
+   Write run.param file for haddock.
+   """
+   # Define list of lines for run.param
+   lines = ["HADDOCK_DIR=/home/oliverh/DockingSoftware/haddock2.4", "N_COMP=2", f"PDB_FILE1={OUTPath}{ab_filename}_clean.pdb", f"PDB_FILE2={OUTPath}{ag_filename}_clean.pdb", "PROJECT_DIR=./", "PROT_SEGID_1=A", "PROT_SEGID_2=B", "RUN_NUMBER=1", "UNAMBIG_TBL=antibody-antigen-unambig.tbl"]
+   # Write run.param file
+   writefile("run.param", lines)
+
+#*************************************************************************
