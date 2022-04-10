4214ef91b38166f62e6f8bdfa9168dd3b1a6453c
oliverhood <51133654+oliverhood@users.noreply.github.com>
2022-04-10 18:27:35 +0100

    Added blocks to write individual results files since something is currently broken with how I have it set up and I NEED RESULTS

--- testdockingprogs_master.py
+++ testdockingprogs_master.py
@@ -238,6 +238,219 @@ scores_all = [MD_all, MD_ca, MD_res_pairs, MD_ab_res, MD_ag_res,
 Piper_all, Piper_ca, Piper_res_pairs, Piper_ab_res, Piper_ag_res, Rosetta_all, Rosetta_ca, Rosetta_res_pairs, Rosetta_ab_res, Rosetta_ag_res
 , ZDOCK_all, ZDOCK_ca, ZDOCK_res_pairs, ZDOCK_ab_res, ZDOCK_ag_res, Hw_all, Hw_ca, Hw_res_pairs, Hw_ab_res, Hw_ag_res, Ha_all, Ha_ca, Ha_res_pairs, Ha_ab_res, Ha_ag_res]
 
+# Write results to individual files (see if this works, something else is going wrong below)
+
+# Write results directory
+subprocess.run([f"mkdir {directory}/results"], shell=True)
+
+# Megadock
+if run_megadock_bool:
+
+   MD_all_out = f"{directory}/results/{inputfilename}_MD_all.txt"
+   MD_ca_out = f"{directory}/results/{inputfilename}_MD_ca.txt"
+   MD_res_pairs_out = f"{directory}/results/{inputfilename}_MD_res_pairs.txt"
+   MD_ab_res_out = f"{directory}/results/{inputfilename}_MD_ab_res.txt"
+   MD_ag_res_out = f"{directory}/results/{inputfilename}_MD_ag_res.txt"
+
+   # MD_all
+   with open(MD_all_out) as file:
+      for line in MD_all:
+         file.write(f"{line} /n")
+
+   # MD_ca
+   with open(MD_ca_out) as file:
+      for line in MD_ca:
+         file.write(f"{line} /n")
+
+   # MD_res_pairs
+   with open(MD_res_pairs_out) as file:
+      for line in MD_res_pairs:
+         file.write(f"{line} /n")
+
+   # MD_ab_res
+   with open(MD_ab_res_out) as file:
+      for line in MD_ab_res:
+         file.write(f"{line} /n")
+
+   # MD_ag_res
+   with open(MD_ag_res_out) as file:
+      for line in MD_ag_res:
+         file.write(f"{line} /n")
+
+# Piper
+if run_piper_bool:
+
+   Piper_all_out = f"{directory}/results/{inputfilename}_Piper_all.txt"
+   Piper_ca_out = f"{directory}/results/{inputfilename}_Piper_ca.txt"
+   Piper_res_pairs_out = f"{directory}/results/{inputfilename}_Piper_res_pairs.txt"
+   Piper_ab_res_out = f"{directory}/results/{inputfilename}_Piper_ab_res.txt"
+   Piper_ag_res_out = f"{directory}/results/{inputfilename}_Piper_ag_res.txt"
+
+   # Piper_all
+   with open(Piper_all_out) as file:
+      for line in Piper_all:
+         file.write(f"{line} /n")
+
+   # Piper_ca
+   with open(Piper_ca_out) as file:
+      for line in Piper_ca:
+         file.write(f"{line} /n")
+
+   # Piper_res_pairs
+   with open(Piper_res_pairs_out) as file:
+      for line in Piper_res_pairs:
+         file.write(f"{line} /n")
+
+   # Piper_ab_res
+   with open(Piper_ab_res_out) as file:
+      for line in Piper_ab_res:
+         file.write(f"{line} /n")
+
+   # MD_ag_res
+   with open(Piper_ag_res_out) as file:
+      for line in Piper_ag_res:
+         file.write(f"{line} /n")
+
+
+# Rosetta
+if run_rosetta_bool:
+
+   Rosetta_all_out = f"{directory}/results/{inputfilename}_Rosetta_all.txt"
+   Rosetta_ca_out = f"{directory}/results/{inputfilename}_Rosetta_ca.txt"
+   Rosetta_res_pairs_out = f"{directory}/results/{inputfilename}_Rosetta_res_pairs.txt"
+   Rosetta_ab_res_out = f"{directory}/results/{inputfilename}_Rosetta_ab_res.txt"
+   Rosetta_ag_res_out = f"{directory}/results/{inputfilename}_Rosetta_ag_res.txt"
+
+   # Rosetta_all
+   with open(Rosetta_all_out) as file:
+      for line in Rosetta_all:
+         file.write(f"{line} /n")
+
+   # Rosetta_ca
+   with open(Rosetta_ca_out) as file:
+      for line in Rosetta_ca:
+         file.write(f"{line} /n")
+
+   # Rosetta_res_pairs
+   with open(Rosetta_res_pairs_out) as file:
+      for line in Rosetta_res_pairs:
+         file.write(f"{line} /n")
+
+   # Rosetta_ab_res
+   with open(Rosetta_ab_res_out) as file:
+      for line in Rosetta_ab_res:
+         file.write(f"{line} /n")
+
+   # Rosetta_ag_res
+   with open(Rosetta_ag_res_out) as file:
+      for line in Rosetta_ag_res:
+         file.write(f"{line} /n")
+
+
+# ZDOCK
+if run_zdock_bool:
+
+   ZDOCK_all_out = f"{directory}/results/{inputfilename}_ZDOCK_all.txt"
+   ZDOCK_ca_out = f"{directory}/results/{inputfilename}_ZDOCK_ca.txt"
+   ZDOCK_res_pairs_out = f"{directory}/results/{inputfilename}_ZDOCK_res_pairs.txt"
+   ZDOCK_ab_res_out = f"{directory}/results/{inputfilename}_ZDOCK_ab_res.txt"
+   ZDOCK_ag_res_out = f"{directory}/results/{inputfilename}_ZDOCK_ag_res.txt"
+
+   # ZDOCK_all
+   with open(ZDOCK_all_out) as file:
+      for line in ZDOCK_all:
+         file.write(f"{line} /n")
+
+   # ZDOCK_ca
+   with open(ZDOCK_ca_out) as file:
+      for line in ZDOCK_ca:
+         file.write(f"{line} /n")
+
+   # ZDOCK_res_pairs
+   with open(ZDOCK_res_pairs_out) as file:
+      for line in ZDOCK_res_pairs:
+         file.write(f"{line} /n")
+
+   # ZDOCK_ab_res
+   with open(ZDOCK_ab_res_out) as file:
+      for line in ZDOCK_ab_res:
+         file.write(f"{line} /n")
+
+   # ZDOCK_ag_res
+   with open(ZDOCK_ag_res_out) as file:
+      for line in ZDOCK_ag_res:
+         file.write(f"{line} /n")
+
+
+# Haddock waters
+if run_haddock_bool:
+
+   Hw_all_out = f"{directory}/results/{inputfilename}_Hw_all.txt"
+   Hw_ca_out = f"{directory}/results/{inputfilename}_Hw_ca.txt"
+   Hw_res_pairs_out = f"{directory}/results/{inputfilename}_Hw_res_pairs.txt"
+   Hw_ab_res_out = f"{directory}/results/{inputfilename}_Hw_ab_res.txt"
+   Hw_ag_res_out = f"{directory}/results/{inputfilename}_Hw_ag_res.txt"
+
+   # Hw_all
+   with open(Hw_all_out) as file:
+      for line in Hw_all:
+         file.write(f"{line} /n")
+
+   # Hw_ca
+   with open(Hw_ca_out) as file:
+      for line in Hw_ca:
+         file.write(f"{line} /n")
+
+   # Hw_res_pairs
+   with open(Hw_res_pairs_out) as file:
+      for line in Hw_res_pairs:
+         file.write(f"{line} /n")
+
+   # Hw_ab_res
+   with open(Hw_ab_res_out) as file:
+      for line in Hw_ab_res:
+         file.write(f"{line} /n")
+
+   # Hw_ag_res
+   with open(Hw_ag_res_out) as file:
+      for line in Hw_ag_res:
+         file.write(f"{line} /n")
+
+
+# Haddock no waters
+if run_haddock_bool:
+
+   Ha_all_out = f"{directory}/results/{inputfilename}_Ha_all.txt"
+   Ha_ca_out = f"{directory}/results/{inputfilename}_Ha_ca.txt"
+   Ha_res_pairs_out = f"{directory}/results/{inputfilename}_Ha_res_pairs.txt"
+   Ha_ab_res_out = f"{directory}/results/{inputfilename}_Ha_ab_res.txt"
+   Ha_ag_res_out = f"{directory}/results/{inputfilename}_Ha_ag_res.txt"
+
+   # Ha_all
+   with open(Ha_all_out) as file:
+      for line in Ha_all:
+         file.write(f"{line} /n")
+
+   # Ha_ca
+   with open(Ha_ca_out) as file:
+      for line in Ha_ca:
+         file.write(f"{line} /n")
+
+   # Ha_res_pairs
+   with open(Ha_res_pairs_out) as file:
+      for line in Ha_res_pairs:
+         file.write(f"{line} /n")
+
+   # Ha_ab_res
+   with open(Ha_ab_res_out) as file:
+      for line in Ha_ab_res:
+         file.write(f"{line} /n")
+
+   # Ha_ag_res
+   with open(Ha_ag_res_out) as file:
+      for line in Ha_ag_res:
+         file.write(f"{line} /n")
+
 # Calculate scores for each method
 avg_scores = []
 lowest_scores = []
