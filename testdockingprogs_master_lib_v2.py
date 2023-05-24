c5e1c7e440aefcf8690250b276d000be64a7b52c
oliverhood <51133654+oliverhood@users.noreply.github.com>
2023-05-24 16:00:09 +0100

    Added modified run_x functions to lib_v2, removed evaluation steps

--- testdockingprogs_master_lib_v2.py
+++ testdockingprogs_master_lib_v2.py
@@ -84,3 +84,73 @@ def run_piper(PDBfile, inputfilename, ab_filename, ag_filename, OUTPath_i):
     # Print complete piper
     print(f"Piper docking completed.")
     print(f"Result file located at {piper_resultfile}")
+
+#*************************************************************************
+
+
+# Rosetta function
+def run_rosetta(PDBfile, inputfilename, ab_filename, ag_filename, OUTPath_i):
+   """"
+   Function to run the Rosetta program.
+
+   """
+   # Starting rosetta
+   print("Starting Rosetta...", end='', flush=True)
+   # Get date and time that method is being run at
+   current_time = time.strftime(r"%d.%m.%Y | %H:%M:%S", time.localtime())
+
+   # Run Rosetta on input files (performing 50 runs within the program)
+   subprocess.run([f"~/ab-docking-scripts/runrosetta.py {PDBfile} {ab_filename} {ag_filename} 50 {OUTPath_i}"], shell=True)
+
+   # Define output filename
+   rosetta_resultfile = OUTPath_i + inputfilename + "_Rosetta_result.pdb"
+
+
+   # Print complete rosetta
+   print(f"Rosetta docking completed.")
+   print(f"Result file located at {rosetta_resultfile}")
+
+#*************************************************************************
+
+# Haddock function
+
+def run_haddock(PDBfile, inputfilename, ab_filename, ag_filename, OUTPath_i, dockingresults, Ha_all, Ha_ca, Ha_res_pairs, Ha_ab_res, Ha_ag_res, Hw_all, Hw_ca, Hw_res_pairs, Hw_ab_res, Hw_ag_res):
+   """
+   Function to run haddock program (with and without waters).
+
+   """
+   # Print starting Haddock
+   print("Starting Haddock...", end='', flush=True)
+   # Get date and time that method is being run at
+   current_time = time.strftime(r"%d.%m.%Y | %H:%M:%S", time.localtime())
+
+   # Define haddock_out directory name
+   haddock_out = f"{OUTPath_i}/haddock_out/"
+   # Create 'Haddock_out' directory
+   subprocess.run([f"mkdir {haddock_out}"], shell=True)
+   # Move input files to haddock_out
+   subprocess.run([f"cp {PDBfile} {ab_filename} {ag_filename} {haddock_out}"], shell=True)
+   # Find current directory
+   cwd = f"{os.getcwd()}/"
+   # Change to haddock_out directory
+   os.chdir(haddock_out)
+
+   # Run Haddock on input files
+   subprocess.run([f"~/ab-docking-scripts/runhaddock.py {ab_filename} {ag_filename} short {haddock_out}"], shell=True)
+
+   # Define output waters filename
+   haddock_waters_resultfile = haddock_out + inputfilename + "_nohydrogens_Haddock_waters_result.pdb_split_labelled.pdb"
+
+   # Define output waters filename
+   haddock_nowaters_resultfile = haddock_out + inputfilename + "_nohydrogens_Haddock_nowaters_result.pdb_split_labelled.pdb"
+
+   # Change back to starting directory
+   os.chdir(cwd)
+
+   # Print complete haddock
+   print(f"Haddock docking completed.")
+   print(f"Result files located at: ")
+   print(f"{haddock_waters_resultfile}")
+   print(f"{haddock_nowaters_resultfile}")
+
+#*************************************************************************
