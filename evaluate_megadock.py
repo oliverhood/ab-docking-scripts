3dee2aa1e71d861d5e46dde3285632be3a172d2b
oliverhood <51133654+oliverhood@users.noreply.github.com>
2023-06-26 16:50:17 +0100

    Changed output format

--- evaluate_megadock.py
+++ evaluate_megadock.py
@@ -62,60 +62,67 @@ evaluation_outputs += [filename_stripped]
 #*************************************************************************
 
 # Evaluate each output
+print(filename_stripped)
+
 # rank1
+print("Rank1:")
 rank_1 = f"{target_dir}/rank_1.pdb"
 
 # Run evaluation
 output_1 = str(subprocess.run([f"~/ab-docking-scripts/evaluate_interface.py {OG_file} {rank_1}"], shell=True))
 
 # save evaluation
-evaluation_outputs += ["Rank 1:"]
-evaluation_outputs += [output_1]
+#evaluation_outputs += ["Rank 1:"]
+#evaluation_outputs += [output_1]
 
 # rank2
+print("Rank2:")
 rank_2 = f"{target_dir}/rank_2.pdb"
 
 # Run evaluation
 output_2 = str(subprocess.run([f"~/ab-docking-scripts/evaluate_interface.py {OG_file} {rank_2}"], shell=True))
 
 # save evaluation
-evaluation_outputs += ["Rank 2:"]
-evaluation_outputs += [output_2]
+#evaluation_outputs += ["Rank 2:"]
+#evaluation_outputs += [output_2]
 
 # rank3
+print("Rank3:")
 rank_3 = f"{target_dir}/rank_3.pdb"
 
 # Run evaluation
 output_3 = str(subprocess.run([f"~/ab-docking-scripts/evaluate_interface.py {OG_file} {rank_3}"], shell=True))
 
 # save evaluation
-evaluation_outputs += ["Rank 3:"]
-evaluation_outputs += [output_3]
+#evaluation_outputs += ["Rank 3:"]
+#evaluation_outputs += [output_3]
 
 # rank4
+print("Rank4:")
 rank_4 = f"{target_dir}/rank_4.pdb"
 
 # Run evaluation
 output_4 = str(subprocess.run([f"~/ab-docking-scripts/evaluate_interface.py {OG_file} {rank_4}"], shell=True))
 
 # save evaluation
-evaluation_outputs += ["Rank 4:"]
-evaluation_outputs += [output_4]
+#evaluation_outputs += ["Rank 4:"]
+#evaluation_outputs += [output_4]
 
-# rank1
+# rank5
+print("Rank5:")
 rank_5 = f"{target_dir}/rank_5.pdb"
 
 # Run evaluation
 output_5 = str(subprocess.run([f"~/ab-docking-scripts/evaluate_interface.py {OG_file} {rank_5}"], shell=True))
 
 # save evaluation
-evaluation_outputs += ["Rank 5:"]
-evaluation_outputs += [output_5]
+#evaluation_outputs += ["Rank 5:"]
+#evaluation_outputs += [output_5]
 
 #*************************************************************************
 
 # Print outputs
 
-for item in evaluation_outputs:
-    print(item)
+#for item in evaluation_outputs:
+#    print(item)
 
